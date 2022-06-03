cfc27af6733c4829b1b6247f33ca076d4f5e4aee
Daniel Stawiarski <danielstawiarski@wp.pl>
2022-06-03 11:06:10 +0200

    Add UDF example

--- src/main/java/com/danielstawiarski/sparkexercise/spark_sql/SparkSqlUDFPlay.java
+++ src/main/java/com/danielstawiarski/sparkexercise/spark_sql/SparkSqlUDFPlay.java
@@ -7,27 +7,30 @@ import org.apache.log4j.Logger;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.types.DataTypes;
 import org.sparkproject.jetty.server.handler.ContextHandler;
 
-/**
- * This exercise serves to generate report with average and standard deviation per year using pivot table
- * for big data file with students results
- */
-public class ExamsResultsReportExercise {
-
+public class SparkSqlUDFPlay {
     public static void main(String[] args) {
+
         Logger.getLogger("org.apache").setLevel(Level.WARN);
         Logger.getLogger(ContextHandler.class).setLevel(Level.WARN);
 
         SparkSession spark = SparkSession.builder().appName("sqlPlay").master("local[*]").getOrCreate();
 
+        spark.udf().register("hasPassed", (String grade, String subject) ->
+        {
+            if (subject.equals("Biology")) {
+                return grade.startsWith("A");
+            }
+            return grade.startsWith("A") || grade.startsWith("B") || grade.startsWith("C");
+        }, DataTypes.BooleanType);
+
         Dataset<Row> dataSet = spark.read().option("header", true).csv("src/main/resources/exams/students.csv");
 
-        dataSet = dataSet.groupBy("subject").pivot("year").agg(
-                round(avg(col("score")), 2).alias("score_average"),
-                round(stddev(col("score")), 2).alias("score_standard_deviation")
-        );
+        dataSet = dataSet.withColumn("passed", callUDF("hasPassed", col("grade"), col("subject")));
 
         dataSet.show();
+
     }
 }
